96da4bfe0dd6910a12d0abec7363bd489259ee44
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2024-05-14 22:32:59 +0530

    Fix sizes and vocabs

--- expr/scripts/train.py
+++ expr/scripts/train.py
@@ -6,9 +6,9 @@ from lightning import Trainer
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
 
-for size in [1e3, 1e4, 1e5, 1e6]:
+for size in [1e3, 1e4, 1e5]:
 
-    config = {'ntoken': 20, 'd_model': 64, 'nhead': 2, 'd_hid': 256, 'nlayers': 3, 'dataset_size' : size}
+    config = {'ntoken': 19, 'd_model': 64, 'nhead': 2, 'd_hid': 256, 'nlayers': 3, 'dataset_size' : size}
     model = TransformerLMLightning(**config, from_pretrained=False)
     early_stopping = EarlyStopping(monitor='val_loss', patience=3)
     model_checkpoint = ModelCheckpoint(monitor='val_loss', save_top_k=-1, dirpath='models/', filename=f'size={size}.ckpt')
